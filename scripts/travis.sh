b5d7ce52e65da1c32a943149c67b4e58e73d2558
neverchanje <wutao1@xiaomi.com>
2019-01-21 14:42:49 +0800

    fix travis.sh

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -20,9 +20,9 @@ shellcheck_must_pass ./scripts/format-all.sh
 shellcheck_must_pass ./scripts/travis.sh
 
 # start pegasus onebox environment
-wget https://github.com/XiaoMi/pegasus/releases/download/v1.11.2/pegasus-server-1.11.2-a186d38-ubuntu-18.04-release.tar.gz
-tar xvf pegasus-server-1.11.2-a186d38-ubuntu-18.04-release.tar.gz
-cd pegasus-server-1.11.2-a186d38-ubuntu-release
+wget https://github.com/XiaoMi/pegasus/releases/download/v1.11.2/pegasus-tools-1.11.2-a186d38-ubuntu-18.04-release.tar.gz
+tar xvf pegasus-tools-1.11.2-a186d38-ubuntu-18.04-release.tar.gz
+cd pegasus-tools-1.11.2-a186d38-ubuntu-release
 ./run.sh start_onebox
 cd ../
 
