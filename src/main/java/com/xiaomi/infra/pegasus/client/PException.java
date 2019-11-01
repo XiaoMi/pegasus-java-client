93b159029fdfcc0b285cf4b2ebf1e8fffca65e34
neverchanje <wutao1@xiaomi.com>
2019-11-01 15:23:20 +0800

    feat: add more exception info on synchronous APIs

--- src/main/java/com/xiaomi/infra/pegasus/client/PException.java
+++ src/main/java/com/xiaomi/infra/pegasus/client/PException.java
@@ -3,6 +3,9 @@
 // can be found in the LICENSE file in the root directory of this source tree.
 package com.xiaomi.infra.pegasus.client;
 
+import com.xiaomi.infra.pegasus.base.error_code;
+import com.xiaomi.infra.pegasus.rpc.ReplicationException;
+
 /**
  * The generic type of exception thrown by all of the Pegasus APIs.
  *
@@ -26,5 +29,20 @@ public class PException extends Exception {
 
   public PException(Throwable cause) {
     super(cause);
+  }
+
+  static PException threadInterrupted(String tableName) {
+    return new PException(
+        new ReplicationException(
+            error_code.error_types.ERR_THREAD_INTERRUPTED,
+            String.format("[table=%s] Thread is interrupted!", tableName)));
+  }
+
+  static PException timeout(String tableName, int timeout) {
+    return new PException(
+        new ReplicationException(
+            error_code.error_types.ERR_TIMEOUT,
+            String.format(
+                "[table=%s, timeout=%dms] Timeout on Future await!", tableName, timeout)));
   }
 }
