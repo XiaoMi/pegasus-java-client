c816e527b7767a16bdbbdccaac1171468a637575
JiaShuo <js982986555@live.com>
2020-04-27 17:20:41 +0800

    fix test

--- src/test/java/com/xiaomi/infra/pegasus/rpc/async/ClusterManagerTest.java
+++ src/test/java/com/xiaomi/infra/pegasus/rpc/async/ClusterManagerTest.java
@@ -30,7 +30,7 @@ public class ClusterManagerTest {
   public void testGetReplicaSession() throws Exception {
     String[] address_list = {"127.0.0.1:1", "127.0.0.1:2", "127.0.0.1:3"};
 
-    ClusterManager testManager = new ClusterManager(1000, 1, false, null, 60, address_list, false);
+    ClusterManager testManager = new ClusterManager(1000, 1, false, null, 60, address_list);
 
     // input an invalid rpc address
     rpc_address address = new rpc_address();
@@ -43,7 +43,7 @@ public class ClusterManagerTest {
   public void testOpenTable() throws Exception {
     // test invalid meta list
     String[] addr_list = {"127.0.0.1:123", "127.0.0.1:124", "127.0.0.1:125"};
-    ClusterManager testManager = new ClusterManager(1000, 1, false, null, 60, addr_list, false);
+    ClusterManager testManager = new ClusterManager(1000, 1, false, null, 60, addr_list);
 
     TableHandler result = null;
     try {
@@ -59,7 +59,7 @@ public class ClusterManagerTest {
     String[] addr_list2 = {
       "127.0.0.1:123", "127.0.0.1:34603", "127.0.0.1:34601", "127.0.0.1:34602"
     };
-    testManager = new ClusterManager(1000, 1, false, null, 60, addr_list2, false);
+    testManager = new ClusterManager(1000, 1, false, null, 60, addr_list2);
     try {
       result = testManager.openTable("hehe", KeyHasher.DEFAULT, 0);
     } catch (ReplicationException e) {
