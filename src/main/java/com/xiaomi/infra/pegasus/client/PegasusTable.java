f3b0fe5331824813cd4c4a9bc33c949f8c446056
JiaShuo <js982986555@live.com>
2020-05-19 10:19:27 +0800

    format

--- src/main/java/com/xiaomi/infra/pegasus/client/PegasusTable.java
+++ src/main/java/com/xiaomi/infra/pegasus/client/PegasusTable.java
@@ -1438,7 +1438,7 @@ public class PegasusTable implements PegasusTableInterface {
   @Override
   public void multiDel(byte[] hashKey, List<byte[]> sortKeys, int timeout) throws PException {
     if (timeout <= 0) timeout = defaultTimeout;
-    int count = sortKeys == null ? 1 : sortKeys.size();
+    int count = sortKeys == null ? 0 : sortKeys.size();
     try {
       asyncMultiDel(hashKey, sortKeys, timeout).get(timeout, TimeUnit.MILLISECONDS);
     } catch (InterruptedException e) {
