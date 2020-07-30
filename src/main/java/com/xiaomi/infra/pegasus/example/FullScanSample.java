9993a59cd496e6f7610ebee6b1d79710dc0e3637
tangguocheng <tangguocheng1@xiaomi.com>
2020-07-30 21:01:08 +0800

    feat: remove noValue

--- src/main/java/com/xiaomi/infra/pegasus/example/FullScanSample.java
+++ src/main/java/com/xiaomi/infra/pegasus/example/FullScanSample.java
@@ -11,17 +11,16 @@ import org.apache.commons.lang3.tuple.Pair;
 /** full scan sample */
 public class FullScanSample {
 
+  private static final String tableName = "temp";
+
   public static void main(String[] args) throws PException, UnsupportedEncodingException {
     PegasusClientInterface client = PegasusClientFactory.createClient(ClientOptions.create());
-    PegasusTableInterface table = client.openTable("temp");
 
     // Set up the scanners.
     ScanOptions scanOptions = new ScanOptions();
     scanOptions.batchSize = 20;
-    // Values can be optimized out during scanning to reduce the workload.
-    scanOptions.noValue = true;
 
-    List<PegasusScannerInterface> scans = client.getUnorderedScanners("temp", 16, scanOptions);
+    List<PegasusScannerInterface> scans = client.getUnorderedScanners(tableName, 16, scanOptions);
     System.out.printf("opened %d scanners%n", scans.size());
 
     // Iterates sequentially.
@@ -35,11 +34,11 @@ public class FullScanSample {
         byte[] hashKey = keys.getLeft();
         byte[] sortKey = keys.getRight();
         cnt++;
-        // get value
-        byte[] value = table.get(hashKey, sortKey, 3000);
         System.out.printf(
             "hashKey = %s, sortKey = %s, value = %s%n",
-            new String(hashKey, "UTF-8"), new String(sortKey, "UTF-8"), new String(value, "UTF-8"));
+            new String(hashKey, "UTF-8"),
+            new String(sortKey, "UTF-8"),
+            new String(pair.getValue(), "UTF-8"));
       }
       System.out.printf("sacn %d rows cost %d ms%n", cnt, System.currentTimeMillis() - start);
     }
