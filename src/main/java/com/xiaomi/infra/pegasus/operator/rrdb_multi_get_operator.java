7691a79aa4d689253dc653c2c8b2acceb82646b0
neverchanje <wutao.as.neverchanje@gmail.com>
2020-08-31 15:00:07 +0800

    refactor: restrict enableBackupRequest to read_operator

--- src/main/java/com/xiaomi/infra/pegasus/operator/rrdb_multi_get_operator.java
+++ src/main/java/com/xiaomi/infra/pegasus/operator/rrdb_multi_get_operator.java
@@ -16,7 +16,7 @@ public class rrdb_multi_get_operator extends client_operator {
       String tableName,
       multi_get_request request,
       long partitionHash) {
-    super(gpid, tableName, partitionHash, true);
+    super(gpid, tableName, partitionHash);
     this.request = request;
   }
 
