d8872d49db1df21014b4cf921e11700d8c5865c8
JiaShuo <js982986555@live.com>
2020-09-04 16:19:26 +0800

    fix comment

--- src/main/java/com/xiaomi/infra/pegasus/rpc/TableOptions.java
+++ src/main/java/com/xiaomi/infra/pegasus/rpc/TableOptions.java
@@ -4,79 +4,76 @@
 package com.xiaomi.infra.pegasus.rpc;
 
 import com.xiaomi.infra.pegasus.client.PegasusClient.PegasusHasher;
-import com.xiaomi.infra.pegasus.operator.client_operator;
-import com.xiaomi.infra.pegasus.rpc.Table.ClientOPCallback;
 import com.xiaomi.infra.pegasus.rpc.async.ClientRequestRound;
 import com.xiaomi.infra.pegasus.rpc.async.TableHandler;
-import com.xiaomi.infra.pegasus.rpc.interceptor.AutoRetryInterceptor;
 
-/** TableOptions is the internal options for opening a Pegasus table. */
+/** TableOptions is for opening a Pegasus table with some feature */
 public class TableOptions {
 
   /**
-   * this class control the retry options after rpc call failed based `Exponential Backoff`
-   *
-   * <p>every rpc call has timeout{@link ClientRequestRound#timeoutMs}, if not set the RetryOptions
-   * when open table, the rpc timeout is equal with request timeout, see the {@link
-   * TableHandler#asyncOperate(client_operator, ClientOPCallback, int)}, otherwise, the timeout is
-   * updated by {@link AutoRetryInterceptor} and is equal with `retryTime`{@link
-   * RetryOptions#retryTimeMs}, or is equal with `remainingTime`{@link
-   * ClientRequestRound#remainingTime} at last time, detail see {@link
-   * AutoRetryInterceptor#before(ClientRequestRound, TableHandler)}
-   *
-   * <p>after call failed, the retry call will delay `delayTimeMs`{@link RetryOptions#delayTimeMs}
-   * and delayTimeMs = random()
+   * this class control the retry options based `Exponential Backoff` after rpc call failed, used by
+   * {@link com.xiaomi.infra.pegasus.rpc.interceptor.AutoRetryInterceptor}
    */
   public static class RetryOptions {
-    private long retryTimeMs;
+    private long tryTimeoutMs;
     private long delayTimeMs;
-    private int maxRetryTime;
+    private int MaxTryCount;
 
-    public RetryOptions(long retryTimeMs, long delayTimeMs) {
-      assert (retryTimeMs > 0 && delayTimeMs >= 0);
-      this.retryTimeMs = retryTimeMs;
+    public RetryOptions(long tryTimeoutMs, long delayTimeMs) {
+      assert (tryTimeoutMs > 0 && delayTimeMs >= 0);
+      this.tryTimeoutMs = tryTimeoutMs;
       this.delayTimeMs = delayTimeMs;
     }
 
     /**
-     * for example:
+     * set the try timeout
      *
-     * <p>user hope the request timeout is 1000ms and pass the options: retryTime=200ms, which means
-     * it will be timeout after 200ms, if the last call has not enough time, the timeout will be
-     * remaining time can guarantee return before 1000ms
-     *
-     * @param retryTimeMs the rpc timeout if enable rpc retry
-     * @return return this
+     * @param tryTimeoutMs try timeout, it should be greater than zero. if set RetryOptions for
+     *     {@link TableOptions}, which means the rpc timeout is updated the try timeout, detail see
+     *     {@link
+     *     com.xiaomi.infra.pegasus.rpc.interceptor.AutoRetryInterceptor#before(ClientRequestRound,
+     *     TableHandler)}
+     * @return this
      */
-    public RetryOptions withRetryTimeMs(long retryTimeMs) {
-      this.retryTimeMs = retryTimeMs;
+    public RetryOptions withTryTimeoutMs(long tryTimeoutMs) {
+      this.tryTimeoutMs = tryTimeoutMs;
       return this;
     }
 
     /**
-     * @param delayTimeMs if on
-     * @return
+     * set base delay time before the new rpc call, the actual delay time base `Exponential
+     * Backoff`, detail see {@link
+     * com.xiaomi.infra.pegasus.rpc.interceptor.AutoRetryInterceptor#getExpDelayTimeMs(int, long)}
+     *
+     * @param delayTimeMs base delay time before new rpc call
+     * @return this
      */
     public RetryOptions withDelayTimeMs(long delayTimeMs) {
       this.delayTimeMs = delayTimeMs;
       return this;
     }
 
-    public RetryOptions withMaxRetryTime(int maxRetryTime) {
-      this.maxRetryTime = maxRetryTime;
+    /**
+     * set max try count
+     *
+     * @param maxTryCount max call count
+     * @return this
+     */
+    public RetryOptions withMaxTryCount(int maxTryCount) {
+      this.MaxTryCount = maxTryCount;
       return this;
     }
 
-    public long retryTimeMs() {
-      return retryTimeMs;
+    public long tryTimeoutMs() {
+      return tryTimeoutMs;
     }
 
     public long delayTimeMs() {
       return delayTimeMs;
     }
 
-    public int maxRetryTime() {
-      return maxRetryTime;
+    public int maxTryCount() {
+      return MaxTryCount;
     }
   }
 
