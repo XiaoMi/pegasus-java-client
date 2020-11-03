44117fd9d410bb582aa89e60c397122daee369e8
levy <zlw5307@163.com>
2020-11-03 18:44:06 +0800

    fix

--- src/main/java/com/xiaomi/infra/pegasus/security/AuthProtocol.java
+++ src/main/java/com/xiaomi/infra/pegasus/security/AuthProtocol.java
@@ -2,6 +2,12 @@ package com.xiaomi.infra.pegasus.security;
 
 import com.xiaomi.infra.pegasus.rpc.async.ReplicaSession;
 
+/**
+ * authentiation protocol
+ **/
 public interface AuthProtocol {
+  /**
+   * begin the authentiate process
+   **/
   void authenticate(ReplicaSession session);
 }
