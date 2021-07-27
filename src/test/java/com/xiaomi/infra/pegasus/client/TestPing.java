43ee66bc3ce38cd7a65f3868c2fb2da704c94409
jiashuo <js982986555@live.com>
2021-07-27 16:20:37 +0800

    update cache

--- src/test/java/com/xiaomi/infra/pegasus/client/TestPing.java
+++ src/test/java/com/xiaomi/infra/pegasus/client/TestPing.java
@@ -13,7 +13,6 @@ import org.junit.Test;
 public class TestPing {
   @Test
   public void testPing() throws PException {
-    System.out.println("HHHHHHHHHHHHHHHHHHHHHHH");
     PegasusClientInterface client =
         PegasusClientFactory.createClient("resource:///pegasus.properties");
     String tableName = "temp";
