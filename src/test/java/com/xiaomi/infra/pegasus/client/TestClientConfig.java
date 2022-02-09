6b8146331603b98de8ab59dae2da8984be559638
olivertang <tangchao5@xiaomi.com>
2022-02-09 19:44:13 +0800

    add more test case

--- src/test/java/com/xiaomi/infra/pegasus/client/TestClientConfig.java
+++ src/test/java/com/xiaomi/infra/pegasus/client/TestClientConfig.java
@@ -5,7 +5,10 @@ package com.xiaomi.infra.pegasus.client;
 
 import com.xiaomi.infra.pegasus.rpc.Table;
 import com.xiaomi.infra.pegasus.rpc.async.ClusterManager;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -26,6 +29,7 @@ public class TestClientConfig {
     Table innerTable = (Table) FieldUtils.readField(table, "table", true);
     Assert.assertEquals(innerTable.getDefaultMaxFetchCount(), testFetchCount);
     Assert.assertEquals(innerTable.getDefaultMaxFetchBytes(), testFetchBytes);
+    client.close();
 
     testFetchCount = 19;
     testFetchBytes = 19999999;
@@ -46,5 +50,42 @@ public class TestClientConfig {
     innerTable = (Table) FieldUtils.readField(table, "table", true);
     Assert.assertEquals(innerTable.getDefaultMaxFetchCount(), testFetchCount);
     Assert.assertEquals(innerTable.getDefaultMaxFetchBytes(), testFetchBytes);
+    client.close();
+
+    testFetchCount = 10;
+    testFetchBytes = 1 * 1024 * 1024;
+    client =
+        (PegasusClient)
+            (PegasusClientFactory.createClient(
+                ClientOptions.builder()
+                    .maxFetchCount(testFetchCount)
+                    .maxFetchBytes(testFetchBytes)
+                    .build()));
+    String testHashKey = "testHashKey_" + System.currentTimeMillis();
+    List<byte[]> sortKeyList = new ArrayList<>();
+    for (int i = 0; i < 11; ++i) {
+      String testSortKey = "testSortKey_" + System.currentTimeMillis();
+      String testValue = "testValue_" + System.currentTimeMillis();
+      client.set("temp", testHashKey.getBytes(), testSortKey.getBytes(), testValue.getBytes());
+      sortKeyList.add(testSortKey.getBytes());
+    }
+    List<Pair<byte[], List<byte[]>>> requestList = new ArrayList<>();
+    requestList.add(Pair.of(testHashKey.getBytes(), sortKeyList));
+    List<HashKeyData> values = new ArrayList<>();
+    client.batchMultiGet("temp", requestList, values);
+    Assert.assertEquals(1, values.size());
+    Assert.assertFalse(values.get(0).allFetched);
+    Assert.assertEquals(values.get(0).values.size(), 10);
+
+    requestList.clear();
+    values.clear();
+    sortKeyList.remove(0);
+    sortKeyList.remove(0);
+    requestList.add(Pair.of(testHashKey.getBytes(), sortKeyList));
+    client.batchMultiGet("temp", requestList, values);
+    Assert.assertEquals(1, values.size());
+    Assert.assertTrue(values.get(0).allFetched);
+    Assert.assertEquals(values.get(0).values.size(), 9);
+    client.close();
   }
 }
