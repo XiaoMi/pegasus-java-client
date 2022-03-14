70add3bead8cabc257a7bd1a31eaf829184833f7
olivertang <tangchao5@xiaomi.com>
2022-03-14 11:33:18 +0800

    add licence to two files

--- src/main/java/com/xiaomi/infra/pegasus/rpc/async/MetaHandler.java
+++ src/main/java/com/xiaomi/infra/pegasus/rpc/async/MetaHandler.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2017, Xiaomi, Inc.  All rights reserved.
+// This source code is licensed under the Apache License Version 2.0, which
+// can be found in the LICENSE file in the root directory of this source tree.
 package com.xiaomi.infra.pegasus.rpc.async;
 
 import com.xiaomi.infra.pegasus.base.error_code;
